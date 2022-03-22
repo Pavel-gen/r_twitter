d77fd88e6950fb6d66f0f6541ba3130f85d36622
Marat <mara_sub@mail.ru>
2022-03-22 17:55:35 +0300

    add comments to thread_head

--- client/ann/src/components/ThreadPage/ThreadPage.js
+++ client/ann/src/components/ThreadPage/ThreadPage.js
@@ -8,7 +8,9 @@ import BackButton from "../BackButton/BackButton";
 const ThreadPage = () => {
   let { id } = useParams();
   const [posts, setPosts] = useState([]);
-  console.log(id);
+  const [coms, setComs] = useState([]);
+  //  console.log(id);
+
   const url = `http://localhost:4000/api/posts/thread/${id}`;
   const navigate = useNavigate();
 
@@ -17,13 +19,34 @@ const ThreadPage = () => {
     const result = await tweets.json();
     setPosts(result);
   }, []);
-  if (posts.thread) {
+
+  useEffect(async () => {
+    if (posts.thread) {
+      console.log(posts.thread);
+      const base_tweet = posts.thread.filter((item) => item.commentTo == null);
+      console.log(base_tweet);
+      const url_coms = `http://localhost:4000/api/posts/coms/${base_tweet[0]._id}`;
+      const request = await fetch(url_coms);
+      const final_req = await request.json();
+
+      setComs(final_req.filter((item) => item.threadId == null));
+    }
+  }, [posts]);
+
+  if (posts.thread && coms) {
+    console.log(coms);
     return (
       <>
         <div className="list_tweets">
           <BackButton content={"Thread"} />
         </div>
         <ListTweet posts={posts.thread} protocol="thread" />
+        <div className="list_tweets ">
+          <div className="com_p">
+            <p>Comments:</p>
+          </div>
+        </div>
+        <ListTweet posts={coms} protocol="likes" />
       </>
     );
   } else {
